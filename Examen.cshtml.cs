63ef98bc74477e203a1ab6f92cea1e96444ec402
Syrine-dev <64745324+Syrine-dev@users.noreply.github.com>
2020-09-16 15:25:23 +0100

    Add files via upload

---
+++ Examen.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionAutoEcole.Views.Produit
+{
+    public class ExamenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
